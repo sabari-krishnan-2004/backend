4bc35af2b69fb864b155f4b2b7f0e276dd980ae2
unknown <psabarikrishnan@gmail.com>
2024-04-21 18:33:11 +0530

    user controller updated

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localfilePath)=>{
+    try {
+        if (! localfilePath) return null
+        //upload on cloudinary
+        const response = await cloudinary.uploader.upload(localfilePath,{
+            resource_type:"auto"
+        })
+        //file has been uploaded successfuly
+        console.log("file has been uploaded on cloudinary",response.url);
+        return response
+
+    } catch (error) {
+        fs.unlinkSync(localfilePath)//remove the locally saved temp file as the upload operation got failed
+        return null
+    }
+}
+
+export {uploadOnCloudinary};

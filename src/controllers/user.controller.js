e6289e4b37580fe7586e6491f752dd7715762c98
unknown <psabarikrishnan@gmail.com>
2024-04-22 21:34:52 +0530

    user controller done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,45 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
 
 const registerUser = asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message:"ok"
+    const {username,email,fullname,password}=req.body
+    console.log("email :",email);
+
+    if([username,email,fullname,password].some((field)=>field?.trim()==="")){
+        throw new ApiError(400,"all fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or:[{username},{email}]
     })
+
+    if(existedUser){
+        throw new ApiError(409,"user with email or username already exists")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400,"cover image is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!avatar) {
+        throw new ApiError(400,"avatar file is required")
+    }
+
+    User.create({
+        fullname,
+        avatar:avatar.url,
+        coverImage:coverImage.url
+    })
+
+
 })
 
 export {registerUser};
